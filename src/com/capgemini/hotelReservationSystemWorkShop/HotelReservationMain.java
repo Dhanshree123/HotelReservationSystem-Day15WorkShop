c3c945699c3d64da2de9328ba40423573f780277
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-10 21:20:38 +0530

    Added Welcome msg to Master

---
+++ src/com/capgemini/hotelReservationSystemWorkShop/HotelReservationMain.java
@@ -0,0 +1,9 @@
+package com.capgemini.hotelReservationSystemWorkShop;
+
+public class HotelReservationMain {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+
+}
